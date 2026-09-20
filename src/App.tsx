e708cb174d1e306bd9c861a49be023ec4aeb6d56
ahmetcankartall <ahmetkrtl45@gmail.com>
2026-09-21 00:54:15 +0300

    fix: center card independently from footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 function App() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-[#D5E1EF]">
-      <div className="flex h-[499px] w-[320px] flex-col items-center rounded-2xl bg-white p-4 shadow-lg">
-        <img
-          className="h-[288px] w-[288px] rounded-2xl"
-          src="/images/image-qr-code.png"
-          alt="QR Code"
-        />
+    <>
+      <main className="flex min-h-screen items-center justify-center bg-[#D5E1EF]">
+        <div className="flex h-[499px] w-[320px] flex-col items-center rounded-2xl bg-white p-4 shadow-lg">
+          <img
+            className="h-[288px] w-[288px] rounded-2xl"
+            src="/images/image-qr-code.png"
+            alt="QR Code"
+          />
 
-        <h1 className="mt-6 text-center text-[22px] leading-[120%] font-bold text-[#1F314F]">
-          Improve your front-end skills by building projects
-        </h1>
+          <h1 className="mt-6 text-center text-[22px] leading-[120%] font-bold text-[#1F314F]">
+            Improve your front-end skills by building projects
+          </h1>
 
-        <p className="mt-4 text-center text-[16px] leading-[140%] tracking-[0.2px] text-[#68778D]">
-          Scan the QR code to visit Frontend Mentor and take your coding skills
-          to the next level
-        </p>
-      </div>
+          <p className="mt-4 text-center text-[15px] leading-[140%] tracking-[0.2px] text-[#68778D]">
+            Scan the QR code to visit Frontend Mentor and take your coding skills
+            to the next level
+          </p>
+        </div>
+      </main>
 
-      <footer className="pt-12 text-center text-[#68778D]">
+      <footer className="text-center text-[#68778D]">
         Challenge by{' '}
         <a
           className="underline hover:text-[#1F314F]"
@@ -39,7 +41,7 @@ function App() {
         </a>
         .
       </footer>
-    </main>
+    </>
   )
 }
 
